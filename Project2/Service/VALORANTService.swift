7e6466f4a49dd342fcb668882d08357ea54da823
Dave Gumba <davegumba@gmail.com>
2023-09-06 16:08:46 -0400

    added support for different language regions

--- Project2/Service/VALORANTService.swift
+++ Project2/Service/VALORANTService.swift
@@ -13,13 +13,24 @@ struct VALORANTService {
     // ar-AE / de-DE / en-US / es-ES / es-MX / fr-FR / id-ID / it-IT / ja-JP / ko-KR / pl-PL / pt-BR / ru-RU / th-TH / tr-TR / vi-VN / zh-CN / zh-TW
     // use enums
     var localeRegion = "en-US"
+    let localeRegionURLComponent = "?language="
+    var localeString: String
+    let supportedRegions = ["ar-AE", "de-DE", "en-US", "es-ES", "es-MX", "fr-FR", "id-ID", "it-IT", "ja-JP", "ko-KR", "pl-PL", "pt-BR", "ru-RU", "th-TH", "tr-TR", "vi-VN", "zh-CN", "zh-TW"]
     
     init() {
-        guard let language = Locale.current.language.languageCode else { return }
+        let identifier = Locale.current.identifier
+        let languageRegionAndIdentifier = identifier.replacingOccurrences(of: "_", with: "-")
+        
+        localeRegion = languageRegionAndIdentifier
+        localeString = localeRegionURLComponent + localeRegion
+        
+        if !supportedRegions.contains(localeString) {
+            localeString = localeRegionURLComponent + "en-US"
+        }
     }
     
     func fetchAgents(completion: @escaping ([VTAgent]) -> Void) {
-        AF.request(Constants.AGENTS_URL).responseDecodable(of: VTAgentData.self) { response in
+        AF.request(Constants.AGENTS_URL + localeString).responseDecodable(of: VTAgentData.self) { response in
             switch response.result {
             case .success(let agentsRawData):
                 let agentsDataFromResponse = agentsRawData.data
@@ -33,7 +44,7 @@ struct VALORANTService {
     }
     
     func fetchMaps(completion: @escaping ([VTMap]) -> Void) {
-        AF.request(Constants.MAPS_URL).responseDecodable(of: VTMapData.self) { response in
+        AF.request(Constants.MAPS_URL + localeString).responseDecodable(of: VTMapData.self) { response in
             switch response.result {
             case .success(let mapRawData):
                 let mapData = mapRawData.data
@@ -46,7 +57,7 @@ struct VALORANTService {
     }
     
     func fetchWeapons(completion: @escaping ([VTWeapon]) -> Void) {
-        AF.request(Constants.WEAPONS_URL).responseDecodable(of: VTWeaponData.self) { response in
+        AF.request(Constants.WEAPONS_URL + localeString).responseDecodable(of: VTWeaponData.self) { response in
             switch response.result {
             case .success(let weaponRawData):
                 let weaponData = weaponRawData.data
