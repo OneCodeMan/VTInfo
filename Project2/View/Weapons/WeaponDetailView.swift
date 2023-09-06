54c039358d5c26c65cf359ed3a9b6e85ecf82173
Dave Gumba <davegumba@gmail.com>
2023-09-06 08:50:41 -0400

    styling on detail view looks better for weapon

--- Project2/View/Weapons/WeaponDetailView.swift
+++ Project2/View/Weapons/WeaponDetailView.swift
@@ -16,16 +16,18 @@ struct WeaponDetailView: View {
                 
                 Text(weapon.displayName)
                     .font(.largeTitle)
+                    .bold()
                 
                 Text(weapon.category)
                     .font(.title3)
                     .italic()
+                    .foregroundColor(.gray)
                     
                 AsyncImage(url: URL(string: weapon.skins[0].displayIcon ?? Constants.NO_IMAGE_FOUND_URL)!, content: { image in
                         image
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: .infinity, maxHeight: 200)
+                            .frame(maxWidth: .infinity, maxHeight: 100)
                     },
                        placeholder: {
                         ProgressView()
@@ -37,7 +39,8 @@ struct WeaponDetailView: View {
                 // Cost
                 HStack {
                     Text("COST")
-                        .font(.system(size: 30, weight: .heavy))
+                        .font(.system(size: 30))
+                        .bold()
                     
                     Text(weapon.shopData?.cost.codingKey.stringValue ?? "")
                         .font(.system(size: 26, weight: .regular))
@@ -45,45 +48,54 @@ struct WeaponDetailView: View {
                 .padding(.bottom, 10)
                 
                 // Weapon Stats
-                HStack {
-                    VStack {
-                        Text("FIRE RATE")
-                            .font(.system(size: 20, weight: .medium))
-                        Text("BULLETS")
-                            .font(.system(size: 20, weight: .medium))
-                        Text("RUN SPEED MULTIPLIER")
-                            .font(.system(size: 20, weight: .medium))
-                        Text("EQUIP TIME")
-                            .font(.system(size: 20, weight: .medium))
-                        Text("RELOAD TIME")
-                            .font(.system(size: 20, weight: .medium))
-                    }
-                    .frame(alignment: .leading)
-                    
-                    VStack {
-                        Text(String(weapon.weaponStats?.fireRate ?? 0.2))
-                        Text(String(weapon.weaponStats?.magazineSize ?? 1))
-                        Text(String(weapon.weaponStats?.runSpeedMultiplier ?? 0.2))
-                        Text(String(weapon.weaponStats?.equipTimeSeconds ?? 0.2))
-                        Text(String(weapon.weaponStats?.reloadTimeSeconds ?? 0.2))
-                    }
-                    .frame(alignment: .trailing)
-                }
-                
-                // Skins
-                VStack {
-                    Text("SKINS")
+                Group {
+                    Text("WEAPON STATS")
                         .font(.title2)
+                        .bold()
+                        .padding()
                     
-                    ForEach(weapon.skins) { skin in
-                        HStack {
-                            CustomAsyncImage(imageURLString: skin.displayIcon ?? Constants.NO_IMAGE_FOUND_URL)
-                            Text(skin.displayName)
+                    HStack(alignment: .center) {
+                        VStack(alignment: .leading) {
+                            Text("FIRE RATE")
+                                .font(.system(size: 20, weight: .medium))
+                            Text("BULLETS")
+                                .font(.system(size: 20, weight: .medium))
+                            Text("RUN SPEED MULTIPLIER")
+                                .font(.system(size: 20, weight: .medium))
+                            Text("EQUIP TIME")
+                                .font(.system(size: 20, weight: .medium))
+                            Text("RELOAD TIME")
+                                .font(.system(size: 20, weight: .medium))
+                        }
+                        
+                        VStack(alignment: .trailing) {
+                            Text(String(weapon.weaponStats?.fireRate ?? 0.2))
+                            Text(String(weapon.weaponStats?.magazineSize ?? 1))
+                            Text(String(weapon.weaponStats?.runSpeedMultiplier ?? 0.2))
+                            Text(String(weapon.weaponStats?.equipTimeSeconds ?? 0.2))
+                            Text(String(weapon.weaponStats?.reloadTimeSeconds ?? 0.2))
                         }
                     }
                 }
-                .padding(.top, 20)
                 
+                Spacer()
+                    .padding(.top, 25)
+                
+                // Skins
+                Group {
+                    Text("SKINS")
+                        .font(.title2)
+                        .bold()
+                    VStack(alignment: .leading) {
+                        ForEach(weapon.skins) { skin in
+                            HStack {
+                                CustomAsyncImage(imageURLString: skin.displayIcon ?? Constants.NO_IMAGE_FOUND_URL)
+                                Text(skin.displayName)
+                            }
+                        }
+                    }
+                    .padding(.top, 20)
+                }
             }
             .padding()
         }
